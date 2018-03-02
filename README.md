6fc584a38595cbbf1f832e527ad620ad26c99ec3
postables <postables@none.com>
2018-03-01 23:39:25 -0800

    Readme update, added a copy of the ipfs verify library i found on github (note I did not create it)

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
-# What is this?
+# Postables-Sidechains (PChains) - WIP
 
 Repository intended for experimentating with linking up a private ethereum network, to the main ethereum network. No claims are made to the "security" and "decentralization" of using this setup. There are pros and cons that one has to make when using a setup like this. While anyone on the private network can use the smart contracts to submit "proposals" to exchange assets to the main network, only the pre-defined sealers for the PoA network are able to authorize those transactions, broadcasting a transaction to a smart contract on the main ethereum network, which then transfers the mainnet assets to the private network users associated mainnet ethereum address.
 
 If one has their own private network, there currently exists very few ways to transfer various assets between the different networks. While technologies like Plasma are incredibly promising, if the private (or side chain) is not intended for public consumption, say, an organizations personal ethereum network, one doesn't neccessarily need the benefits that a decentralized technology like plasma offers, that combined with the fact that production ready Plasma implementations aren't here, but production ready private ethereum networks are being used is what inspired me to work on this project.
 
 The actual work is done in a private repository I maintain, which is periodically synced with this one.
+
+# Architecture
+
+The overall architecture follows a "relay" principle, in which anyone who has locked up the appropriate amount of mainnet ethereum as a stake, and incentivization method to help deter malicious actors, act as relays. Relays are used to relay data between the mainnet and private networks. Data from here on out can either refer to tokens, tokenized assets, or data payloads. Data payloads is the "data" included in `msg.data` when transactions are made to contracts. The goal behind this is to allow the execution of mainnet contract functions from the private network, or the execution of private network contract functions from the mainnet.
+
+The Architecture section will be broken up into two parts, one part detailing the relaying of tokens, and other similar assets. The second part will deal with the reelaying of data payloads
+
+
+PChains utilize the following software suites, protocols, technologies, etc:
+- IPFS
+- Ethereum Mainnet
+- Ethereum Private Networks (Clique, PoA)
+- Python
+- Solidity
+- web3
+- geth
+
+## Architecture: Data Paylods
+
+There will be a contract on the private network, which users can use to send data payloads to a particular contract on the mainnet. When the data payload is submitted to the contract, and event will be broadcast containing the payload. Relay's will pick this up on a first come first serve basis (this is highly inefficient, and will be improved over time), and submit three pieces of data to IPFS (If a relay detects that the original copy of the data payload was already uploaded, it will cease further processing).
+
+1) An *exact* copy of the data paylaod
+2) A copy of the data payload signed with the relays private ethereum network address
+3) A copy of the data payload signed with the relays public ethereum network address
+
+The relay will then construct a transaction, and submit it to the payload accumulator on the mainnet. The data payload will be held in the accumulator an time `T`. Until time `T` has passed, *anyone* may submit a fraud proof to the accumulator, proving that the data paylaod passed on by the relayer, was fradulent. To mitigate spam attacks, and abuse, a temporary deposit will be made. If the fraud proof attempt was invalid, the deposit will be distributed to the various relayers. In such a situation in which malicious payload data was relayed, the node that relayed the data will be removed from the network as a node, and the ethereum that was staked to become a relayer will be awarded to the submitter of the fraud proof, along with the submitters temporary deposit.
+
+After a relay has sent a transaction to the payload accumulator, an event will be sent out, notifying all relayers, upon which they will download the original versin of the data payload which was uploaded to IFPS, and pin it locally for persistent storage.
+
---
+++ solidity/IpfsVerify.sol
@@ -0,0 +1,128 @@
+pragma solidity 0.4.20;
+
+contract IpfsVerify {
+  /// @title verifyIPFS
+  /// @author Martin Lundfall (martin.lundfall@consensys.net)
+  bytes constant prefix1 = hex"0a";
+  bytes constant prefix2 = hex"080212";
+  bytes constant postfix = hex"18";
+  bytes constant sha256MultiHash = hex"1220";
+  bytes constant ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+
+  /// @dev generates the corresponding IPFS hash (in base 58) to the given string
+  /// @param contentString The content of the IPFS object
+  /// @return The IPFS hash in base 58
+  function generateHash(string contentString) public pure returns (bytes) {
+    bytes memory content = bytes(contentString);
+    bytes memory len = lengthEncode(content.length);
+    bytes memory len2 = lengthEncode(content.length + 4 + 2*len.length);
+    return toBase58(concat(sha256MultiHash, toBytes(sha256(prefix1, len2, prefix2, len, content, postfix, len))));
+  }
+
+  /// @dev Compares an IPFS hash with content
+  function verifyHash(string contentString, string hash) public pure returns (bool) {
+    return equal(generateHash(contentString), bytes(hash));
+  }
+  
+  
+  /// @dev Converts hex string to base 58
+  function toBase58(bytes source) public pure returns (bytes) {
+    if (source.length == 0) return new bytes(0);
+        uint8[] memory digits = new uint8[](40); //TODO: figure out exactly how much is needed
+        digits[0] = 0;
+        uint8 digitlength = 1;
+        for (uint256 i = 0; i<source.length; ++i) {
+            uint carry = uint8(source[i]);
+            for (uint256 j = 0; j<digitlength; ++j) {
+                carry += uint(digits[j]) * 256;
+                digits[j] = uint8(carry % 58);
+                carry = carry / 58;
+            }
+            
+            while (carry > 0) {
+                digits[digitlength] = uint8(carry % 58);
+                digitlength++;
+                carry = carry / 58;
+            }
+        }
+        //return digits;
+        return toAlphabet(reverse(truncate(digits, digitlength)));
+    }
+
+  function lengthEncode(uint256 length) public pure returns (bytes) {
+    if (length < 128) {
+      return to_binary(length);
+    }
+    else {
+      return concat(to_binary(length % 128 + 128), to_binary(length / 128));
+    }
+  }
+
+  function toBytes(bytes32 input) public pure returns (bytes) {
+    bytes memory output = new bytes(32);
+    for (uint8 i = 0; i<32; i++) {
+      output[i] = input[i];
+    }
+    return output;
+  }
+    
+  function equal(bytes memory one, bytes memory two)  public pure returns (bool) {
+    if (!(one.length == two.length)) {
+      return false;
+    }
+    for (uint256 i = 0; i<one.length; i++) {
+      if (!(one[i] == two[i])) {
+	return false;
+      }
+    }
+    return true;
+  }
+
+    function truncate(uint8[] array, uint8 length) public pure returns (uint8[]) {
+        uint8[] memory output = new uint8[](length);
+        for (uint256 i = 0; i<length; i++) {
+            output[i] = array[i];
+        }
+        return output;
+    }
+    
+    function reverse(uint8[] input) public pure returns (uint8[]) {
+        uint8[] memory output = new uint8[](input.length);
+        for (uint256 i = 0; i<input.length; i++) {
+            output[i] = input[input.length-1-i];
+        }
+        return output;
+    }
+    
+    function toAlphabet(uint8[] indices) public pure returns (bytes) {
+        bytes memory output = new bytes(indices.length);
+        for (uint256 i = 0; i<indices.length; i++) {
+            output[i] = ALPHABET[indices[i]];
+        }
+        return output;
+    }
+
+  function concat(bytes byteArray, bytes byteArray2) public pure returns (bytes) {
+    bytes memory returnArray = new bytes(byteArray.length + byteArray2.length);
+    for (uint16 i = 0; i < byteArray.length; i++) {
+      returnArray[i] = byteArray[i];
+    }
+    for (i; i < (byteArray.length + byteArray2.length); i++) {
+      returnArray[i] = byteArray2[i - byteArray.length];
+    }
+    return returnArray;
+  }
+    
+  function to_binary(uint256 x) public pure returns (bytes) {
+    if (x == 0) {
+      return new bytes(0);
+    }
+    else {
+      byte s = byte(x % 256);
+      bytes memory r = new bytes(1);
+      r[0] = s;
+      return concat(to_binary(x / 256), r);
+    }
+  }
+  
+}
