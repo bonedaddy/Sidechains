b77ba343c71512f723580b97568c940523582dfb
postables <postables@none.com>
2018-03-05 04:17:01 -0800

    Updated IPFS backend

--- python/ipfs_backend.py
+++ python/ipfs_backend.py
@@ -3,17 +3,6 @@ from Modules import listener
 from time import sleep
 import sys
 
-# special web 3 overrides to work on a PoA network
-from web3.middleware.pythonic import (
-    pythonic_middleware,
-    to_hexbytes,
-)
-
-size_extraData_for_poa = 200   # can change
-pythonic_middleware.__closure__[2].cell_contents['eth_getBlockByNumber'].args[1].args[0]['extraData'] = to_hexbytes(size_extraData_for_poa, variable_length=True)
-pythonic_middleware.__closure__[2].cell_contents['eth_getBlockByHash'].args[1].args[0]['extraData'] = to_hexbytes(size_extraData_for_poa, variable_length=True)
-#####
-
 # python3.6 ipfs_backend.py <ip> <port>
 if len(sys.argv) > 3 or len(sys.argv) < 3:
 	print("Improper invocation")
