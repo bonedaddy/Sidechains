4fe95f6bc2bb049542cf8260a46322d9d6f93dc3
postables <postables@none.com>
2018-03-05 06:29:18 -0800

    Added the basic workign example of the python data bridge which can detect approvals of data swaps, and upload the file to IPFS

---
+++ python/PrivateDataBridge.py
@@ -0,0 +1,109 @@
+from time import sleep
+from Modules import listener
+from Modules import IpfsModule
+from web3 import Web3
+import datetime
+import json
+import yaml
+
+# lets load the configuration file
+configFile = 'configs/settings_test.yml'
+with open(configFile, 'r') as ymlFile:
+	cfg = yaml.load(ymlFile)
+
+
+pDataBridgeAddress = cfg['private']['dataBridgeAddress']
+pDataBridgeAbiFile = cfg['private']['dataBridgeAbi']
+pSealerAddress = cfg['private']['sealerAddress']
+pSealerPassword = cfg['private']['sealerPassword']
+pConnectionMethod = cfg['private']['connectionMethod']
+
+ipfsIp = cfg['ipfs']['ip']
+ipfsPort = cfg['ipfs']['port']
+
+ipfs = IpfsModule.Ipfs(ipfsIp, ipfsPort)
+ipfs.connect()
+
+
+if pConnectionMethod == 'rpc':
+	pConnectionPath = cfg['private']['rpcUrl']
+elif pConnectionMethod == 'ipc':
+	pConnectionPath = cfg['private']['ipcPath']
+
+
+# lets create a connection object ot the private networl
+pNet = listener.Listener(pSealerAddress, pSealerPassword, pDataBridgeAddress, pDataBridgeAbiFile)
+
+
+if pConnectionMethod == 'rpc':
+	# connect to the network
+	pNet.establishRpcConnection(pConnectionPath)
+elif pConnectionMethod == 'ipc':
+	# connect to the network
+	pNet.establishIpcConnection(pConnectionPath)
+
+
+# before we do anything we need to unlock the account
+pNet.unlockAccount()
+
+# load the bridge contract object
+pNet.loadContract()
+
+
+"""
+	event DataSwapApproved(
+		address _mAddress,
+		address _mContract,
+		bytes   _payload);
+
+How to decode the payload data 
+Web3.toHex(Web3.toBytes(payload))
+
+ipfs file:
+mAddress: 	....
+mContract:	....
+payload:	....
+
+"""
+eventName = 'DataSwapApproved'
+# lets return an  event filter
+eventFilter = pNet.returnEventHandler(eventName)
+
+events = []
+
+swapObjectList = []
+swapObjects = {}
+while True:
+	ev = pNet.getNewEventEntries(eventFilter)
+	if len(ev) > 0:
+		for e in ev:
+			events.append(e)
+	if len(events) > 0:
+		for ev in events:
+			swapObjects = {}
+			for key in ev['args'].keys():
+				if key == '_payload':
+					swapObjects[key.strip('_')] = Web3.toHex(Web3.toBytes(ev['args'][key]))
+				else:
+					swapObjects[key.strip('_')] = ev['args'][key]
+			swapObjectList.append(swapObjects)
+			print("Swap Object List")
+		for swap in swapObjectList:
+			print("Swaps in SwapObjectList")
+			print(swap)
+			now = datetime.datetime.now()
+			date = "%s-%s-%s--%s:%s:%s" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+			msg = "mAddress: %s\nmContract: %s\npayload: %s\n" % (swap['mAddress'], swap['mContract'], swap['payload'])
+			fileName = "%s_%s_%s" % (swap['mAddress'], swap['mContract'], date)
+			with open('/tmp/%s' % fileName, 'w') as fh:
+				fh.write(msg)
+			with open('/tmp/%s' % fileName, 'rb') as fh:
+				ipfs.add_file(fh)
+				print(ipfs.hashes)
+			exit()
+	#print(swapObjects)
+	#print(swapObjectList)
+	sleep(5)
+"""
+AttributeDict({'_mAddress': '0xabB36c583E9B15736038858bDd540f7f422Db0F8', '_recipient': '0x365b3b4d3168a11291449A015FA0C1b34B0B3d72', '_token': '0x28605Eacf39C906b5331C4C81b9BC3c07F3eF606', '_amount': 9999999999999999999})
+"""
--- python/PrivateTokenBridge.py
+++ python/PrivateTokenBridge.py
@@ -53,6 +53,9 @@ eventName = 'SwapApproved'
 eventFilter = pNet.returnEventHandler(eventName)
 
 events = []
+
+swapObjects = {}
+
 while True:
 	ev = pNet.getNewEventEntries(eventFilter)
 	if len(ev) > 0:
@@ -61,4 +64,17 @@ while True:
 	if len(events) > 0:
 		for ev in events:
 			print(ev['args'])
+			for key in ev['args'].keys():
+				if key == '_mAddress':
+					swapObjects['mAddress'] = ev['args'][key]
+				elif key == '_recipient':
+					swapObjects['recipient'] = ev['args'][key]
+				elif key == '_token':
+					swapObjects['token'] = ev['args'][key]
+				elif key == '_amount':
+					swapObjects['amount'] = ev['args'][key]
+			print(swapObjects)
 	sleep(5)
+"""
+AttributeDict({'_mAddress': '0xabB36c583E9B15736038858bDd540f7f422Db0F8', '_recipient': '0x365b3b4d3168a11291449A015FA0C1b34B0B3d72', '_token': '0x28605Eacf39C906b5331C4C81b9BC3c07F3eF606', '_amount': 9999999999999999999})
+"""
